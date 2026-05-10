b18feeafbf6d99322163671c4344e7db06b4e9db
minhajul islam <nishatislam3108@gamil.com>
2026-05-10 14:11:57 +0600

    build simple credentials-create form already. and now we will handle the data part of the form

--- apps/frontend/src/routes/credentials/create/index.tsx
+++ apps/frontend/src/routes/credentials/create/index.tsx
@@ -6,62 +6,87 @@ export const Route = createFileRoute("/credentials/create/")({
 	component: RouteComponent,
 });
 
-// const credentialsSchema = z.object({
-// 	title: z
-// 		.string()
-// 		.trim()
-// 		.min(4, "credentials title need to be at least 4 characters")
-// 		.max(30, "credentials title can not be grater than 30 characters"),
-// 	short_description: z
-// 		.string()
-// 		.min(5, "credentials short description can not be less than 5 characters")
-// 		.max(
-// 			50,
-// 			"credentials short description can not be grater than 50 characters",
-// 		)
-// 		.optional(),
-// 	long_description: z
-// 		.string()
-// 		.min(5, "credentials long description can not be less than 5 characters")
-// 		.optional(),
-// 	thumbnail: z
-// 		.file()
-// 		.max(3000000, "credentials thumbnail can not be larger than 3MB")
-// 		.mime(["image/png", "image/jpg", "image/jpeg", "image/webp"])
-// 		.optional(),
-// 	data: z.string(),
-// 	images: z.string().optional(),
-// 	notes: z.string().optional(),
-// 	tags: z.string().optional(),
-// });
+const singleLabelSchema = z.object({
+	type: z.literal("single_label"),
+	value: z
+		.string()
+		.trim()
+		.min(1, "label value can not be empty")
+		.or(z.literal("")),
+});
 
-// type CredentialCreateType = {
-// 	title: string;
-// 	short_description: string;
-// 	long_description: string;
-// 	thumbnail: File | string;
-// 	data: string;
-// 	images: string;
-// 	notes: string;
-// 	tags: string;
-// };
+const keyValueSchema = z.object({
+	type: z.literal("key_value"),
+	key: z.string().min(1, "key is required"),
+	value: z.string().trim().min(1, "value is required"),
+});
 
-const defaultCredentialValue = {
+const informationSchema = z.object({
+	type: z.literal("information"),
+	text: z.string().trim().min(1, "information text can not be empty"),
+});
+
+const dataBlockSchema = z.discriminatedUnion("type", [
+	singleLabelSchema,
+	keyValueSchema,
+	informationSchema,
+]);
+
+const credentialsCreateSchema = z.object({
+	title: z
+		.string()
+		.trim()
+		.min(4, "credentials title need to be at least 4 characters")
+		.max(30, "credentials title can not be grater than 30 characters"),
+	short_description: z
+		.string()
+		.min(5, "credentials short description can not be less than 5 characters")
+		.max(
+			50,
+			"credentials short description can not be grater than 50 characters",
+		)
+		.optional()
+		.or(z.literal("")),
+	long_description: z
+		.string()
+		.min(5, "credentials long description can not be less than 5 characters")
+		.optional()
+		.or(z.literal("")),
+	thumbnail: z
+		.file()
+		.max(3_000_000, "Max 3mb")
+		.mime(["image/jpg", "image/jpeg", "image/png", "image/webp"])
+		.nullable()
+		.optional(),
+	data: z.array(dataBlockSchema),
+	images: z.array(z.file()).default([]).nullable().optional(),
+	notes: z.string().trim().optional(),
+	tags: z.string().trim().optional(),
+});
+
+type CredentialCreateType = z.infer<typeof credentialsCreateSchema>;
+
+const defaultCredentialValues: CredentialCreateType = {
 	title: "",
-	// short_description: "",
-	// long_description: "",
-	// thumbnail: "",
-	// data: "",
-	// images: "",
-	// notes: "",
-	// tags: "",
+	short_description: undefined,
+	long_description: undefined,
+	thumbnail: null,
+	data: [{ type: "single_label", value: "" }],
+	images: [],
+	notes: "",
+	tags: "",
 };
 
 function RouteComponent() {
 	const form = useForm({
-		defaultValues: defaultCredentialValue,
-		// validators: credentialsSchema,
+		defaultValues: defaultCredentialValues,
+		validators: { onBlur: credentialsCreateSchema },
+		onSubmitMeta: {
+			submitAction: "continue",
+		},
 		onSubmit: async ({ value }) => {
+			// we will construct the FormData here and append files manually
+			// and omit content-type totally???
 			const response = await fetch(
 				`${import.meta.env.VITE_BACKEND_APP}/credentials/create`,
 				{
@@ -85,7 +110,9 @@ function RouteComponent() {
 					e.preventDefault();
 					form.handleSubmit();
 				}}
+				className="flex flex-col gap-4"
 			>
+				{/*title field*/}
 				<form.Field
 					name="title"
 					children={(field) => (
@@ -98,11 +125,161 @@ function RouteComponent() {
 									value={field.state.value}
 									onBlur={field.handleBlur}
 									onChange={(e) => field.handleChange(e.target.value)}
+									className="ring-1 ml-2 w-80"
 								/>
 							</div>
 						</>
 					)}
 				/>
+				{/*side-by-side short and long desciption*/}
+				<div className="flex gap-4 justify-between">
+					{/*short description*/}
+					<form.Field
+						name="short_description"
+						children={(field) => (
+							<>
+								<div className="flex flex-col w-full">
+									<label htmlFor="short_description">short description</label>
+									<textarea
+										rows={6}
+										cols={33}
+										id="short_description"
+										name="short_description"
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										className="ring-1 mx-1"
+									/>
+								</div>
+							</>
+						)}
+					/>
+					{/*long description*/}
+					<form.Field
+						name="long_description"
+						children={(field) => (
+							<>
+								<div className="flex flex-col w-full">
+									<label htmlFor="long_description">long description</label>
+									<textarea
+										rows={6}
+										cols={33}
+										id="long_description"
+										name="long_description"
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										className="ring-1 mx-1"
+									/>
+								</div>
+							</>
+						)}
+					/>
+				</div>
+				{/*thumbnail*/}
+				<form.Field
+					name="thumbnail"
+					children={(field) => (
+						<>
+							<div>
+								<label htmlFor="thumbnail">
+									thumbnail (to describe the data as image)
+								</label>
+								<input
+									id="thumbnail"
+									name="thumbnail"
+									type="file"
+									onBlur={field.handleBlur}
+									onChange={(e) => {
+										const file = e.target.files?.[0] || null;
+										field.handleChange(file);
+									}}
+									className="ring-1 ml-2 w-80"
+								/>
+							</div>
+						</>
+					)}
+				/>
+
+				{/* data placeholder*/}
+				<div className="text-3xl my-12 text-center italic">
+					data placeholder
+				</div>
+				{/*images*/}
+				<form.Field
+					name="images"
+					children={(field) => (
+						<>
+							<div>
+								<label htmlFor="images">
+									images (to describe the data as image)
+								</label>
+								<input
+									id="images"
+									name="images"
+									type="file"
+									onBlur={field.handleBlur}
+									onChange={(e) => {
+										const files = e.target.files
+											? Array.from(e.target.files)
+											: [];
+										field.handleChange(files);
+									}}
+									multiple
+									className="ring-1 ml-2 w-80"
+								/>
+							</div>
+						</>
+					)}
+				/>
+				{/*side-by-side notes and tags*/}
+				<div className="flex justify-between w-full gap-4">
+					{/*notes*/}
+					<form.Field
+						name="notes"
+						children={(field) => (
+							<>
+								<div className="flex flex-col w-full">
+									<label htmlFor="notes">notes</label>
+									<textarea
+										rows={6}
+										cols={33}
+										id="notes"
+										name="notes"
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										className="ring-1 mx-1"
+									/>
+								</div>
+							</>
+						)}
+					/>
+					{/*tags*/}
+					<form.Field
+						name="tags"
+						children={(field) => (
+							<>
+								<div className="flex flex-col w-full">
+									<label htmlFor="tags">tags</label>
+									<textarea
+										rows={6}
+										cols={33}
+										id="tags"
+										name="tags"
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										className="ring-1 mx-1"
+									/>
+								</div>
+							</>
+						)}
+					/>
+				</div>
+				<button type="submit" className="my-4 text-2xl">
+					add new credential data
+				</button>
 			</form>
 		</main>
 	);
